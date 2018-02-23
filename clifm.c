eac291781042c2722b7ccd2198b8e3b061349d06
leo-arch <leonardoabramovich2@gmail.com>
2018-02-23 16:25:48 -0300

    Source code

--- clifm.c
+++ clifm.c
@@ -1,22 +1,4 @@
 //kliss(fm) (cli+kiss+bliss)?
-/*
-###LICENSE NOTICE###
-Copyright (c) 2017, L. Abramovich
-
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License (version 2) as 
-   published by the Free Software Foundation.
-
-   CliFM is distributed in the hope that it will be useful, but WITHOUT 
-   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
-   FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
-   for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program. If not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
-   02110-1301, USA, or visit <http://www.gnu.org/licenses/>
-*/
 
 /*NOTE:
 Though this program was never intended to be a shell, it might be easily used as such:
