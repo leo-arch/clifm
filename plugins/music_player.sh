ade72f522f1c4405affd7aa83d3effd96b353916
archcrack <johndoe.arch@outlook.com>
2021-02-26 03:58:55 -0300

    Add music playlist plugin

---
+++ plugins/music_player.sh
@@ -0,0 +1,30 @@
+#!/bin/env bash
+
+# Music player plugin for CliFM
+# Written by L. Abramovich
+
+SUCCESS=0
+ERROR=1
+
+if [[ -z "$1" ]]; then
+	echo "CliFM: Missing argument" >&2
+	exit $ERROR
+fi
+
+TMP_FILE="/tmp/clifm/playlist.$(tr -dc A-Za-z0-9 </dev/urandom | head -c 6)"
+
+for file in "$@"; do
+	if [[ ${file[0]} != '/' ]]; then
+		echo "$PWD/$file" | sed 's/\\//g' >> "$TMP_FILE"
+	else
+		echo "$file" | sed 's/\\//g' >> "$TMP_FILE"
+	fi
+done
+
+"${EDITOR:=nano}" "$TMP_FILE"
+
+mplayer -playlist "$TMP_FILE"
+
+rm -rf -- "$TMP_FILE"
+
+exit $SUCCESS
