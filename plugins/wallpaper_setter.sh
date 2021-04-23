ee67d76ecf99aea2aace6c7bdd3624a28fcf5565
archcrack <johndoe.arch@outlook.com>
2021-04-23 01:57:56 -0300

    Update wallsetter plugin

--- plugins/wallpaper_setter.sh
+++ plugins/wallpaper_setter.sh
@@ -3,14 +3,13 @@
 # Wallpaper setter plugin for CliFM
 # Written by L. Abramovich
 
+# Dependencies: file, and feh, xsetbg (xloadimage), or hsetroot
+
 SUCCESS=0
 ERROR=1
 
-WALLSETTER="feh"
-OPTS="--no-fehbg --bg-fill"
-
 if [ -z "$1" ]; then
-	printf "CliFM: Missing argument\n" >&2
+	printf "CliFM: You must specify an image file\n" >&2
 	exit $ERROR
 fi
 
@@ -19,17 +18,22 @@ if ! [ "$(which file 2>/dev/null)" ]; then
 	exit $ERROR
 fi
 
-if ! [ "$(which "$WALLSETTER" 2>/dev/null)" ]; then
-	printf "CliFM: %s: Command not found\n" "$WALLSETTER" >&2
-	exit $ERROR
-fi
-
 if ! [ "$(file -bi "$1" | grep "image/")" ]; then
 	printf "CliFM: %s: Not an image file\n" "$1" >&2
 	exit $ERROR
 fi
 
-"$WALLSETTER" $OPTS "$1"
+if [ "$(which feh 2>/dev/null)" ]; then
+	feh --no-fehbg --bg-fill "$1"
+elif [ "$(which xsetbg 2>/dev/null)" ]; then
+	xsetbg -center "$1"
+elif [ "$(which hsetroot 2>/dev/null)" ]; then
+	hsetroot -center "$1"
+else
+	printf "CliFM: No wallpaper setter found. Install either feh, \
+xloadimage, or hsetroot\n" >&2
+	exit $ERROR
+fi
 
 if [ $? -eq 0 ]; then
 	exit $SUCCESS
