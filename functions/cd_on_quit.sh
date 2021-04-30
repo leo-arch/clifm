0277071ae16545726292f1ec62555255bc07bc32
archcrack <johndoe.arch@outlook.com>
2021-04-30 20:21:46 -0300

    Fix last dir file parsing

--- functions/cd_on_quit.sh
+++ functions/cd_on_quit.sh
@@ -1,17 +1,19 @@
-# CLiFM CD on quit function
+# CliFM CD on quit function
 
 # Run CliFM and, after exit, read the .last file and cd into it
 
-# 1) Customize this function as you need and add it to your .bashrc file
+# 1) Customize this function as you need and add it to your shell configuration
+# file (e.g., $HOME/.bashrc)
 # 2) Restart your shell (for changes to take effect)
 # 3) Run clifm using the name of the function below: c [ARGS ...]
 
+# CliFM CD on quit function
 c() {
 	clifm "--cd-on-quit" "$@"
-	dir=$(cat "${XDG_CONFIG_HOME:=${HOME}/.config}/clifm/.last" 2>/dev/null);
-	if [[ -d "$dir" ]]; then
+	dir="$(grep ^"*" "${XDG_CONFIG_HOME:=${HOME}/.config}/clifm/.last" 2>/dev/null | cut -d':' -f2)";
+	if [ -d "$dir" ]; then
 		cd "$dir"
 	else
-		echo "No directory specified" >&2
+		printf "No directory specified\n" >&2
 	fi
 }
