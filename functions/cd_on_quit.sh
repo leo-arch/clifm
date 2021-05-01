97e0b26e09606ccb2eeaf7db2f6ec8d6a7a72c90
archcrack <johndoe.arch@outlook.com>
2021-05-01 03:26:29 -0300

    Shellcheck functions

--- functions/cd_on_quit.sh
+++ functions/cd_on_quit.sh
@@ -1,18 +1,20 @@
+# shellcheck shell=sh
+
 # CliFM CD on quit function
 
+# Description
 # Run CliFM and, after exit, read the .last file and cd into it
 
-# 1) Customize this function as you need and add it to your shell configuration
-# file (e.g., $HOME/.bashrc)
+# 1) Customize this function as you need and add copy it or source it from your shell configuration file (e.g., .bashrc, .zshrc, etc)
 # 2) Restart your shell (for changes to take effect)
 # 3) Run clifm using the name of the function below: c [ARGS ...]
 
 # CliFM CD on quit function
 c() {
 	clifm "--cd-on-quit" "$@"
-	dir="$(grep ^"*" "${XDG_CONFIG_HOME:=${HOME}/.config}/clifm/.last" 2>/dev/null | cut -d':' -f2)";
+	dir="$(grep "^\*" "${XDG_CONFIG_HOME:=${HOME}/.config}/clifm/.last" 2>/dev/null | cut -d':' -f2)";
 	if [ -d "$dir" ]; then
-		cd "$dir"
+		cd "$dir" || return 1
 	else
 		printf "No directory specified\n" >&2
 	fi
--- functions/file_picker.sh
+++ functions/file_picker.sh
@@ -1,3 +1,5 @@
+# shellcheck shell=sh
+
 # CLiFM file picker function
 
 # Usage: p CMD [ARGS ...], where CMD (plus its corresponding parameters)# is the command that should pick selected files. Example: p ls -l.
@@ -9,23 +11,24 @@
 
 p() {
 
-	if [[ -z "$1" ]]; then
-		echo "Usage: p CMD [ARGS ...]" >&2
+	if [ -z "$1" ]; then
+		printf "Usage: p CMD [ARGS ...]\n" >&2
 		return
 	fi
 
 	CLIFM_OPTIONS=""
 
+	# shellcheck disable=SC2086
 	clifm $CLIFM_OPTIONS
-	file="$(find ${XDG_CONFIG_HOME:=${HOME}/.config}/clifm -name selbox*)"
-	if [[ -z "$file" ]]; then
-		file="$(find /tmp/clifm -name selbox*)"
+	file="$(find "${XDG_CONFIG_HOME:=${HOME}/.config}/clifm" -name 'selbox*')"
+	if [ -z "$file" ]; then
+		file="$(find /tmp/clifm -name 'selbox*')"
 	fi
 
-	if [[ -f "$file" ]]; then
-		"$@" $(cat "$file")
+	if [ -f "$file" ]; then
+		"$@" "$(cat "$file")"
 		rm -- "$file"
 	else
-		echo "No selected files" >&2
+		printf "No selected files\n" >&2
 	fi
 }
