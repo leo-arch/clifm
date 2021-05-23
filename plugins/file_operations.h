1cacd1263a1dbd8b644b875b67f911debccda411
leo-arch <leonardoabramovich2@gmail.com>
2021-05-23 20:15:08 -0300

    Add create file command

---
+++ plugins/file_operations.h
@@ -0,0 +1,36 @@
+/* file_operations.h */
+
+/*
+ * This file is part of CliFM
+ * 
+ * Copyright (C) 2016-2021, L. Abramovich <johndoe.arch@outlook.com>
+ * All rights reserved.
+
+ * CliFM is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * CliFM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+*/
+
+#pragma once
+
+/* file_operations.c */
+int batch_link(char **args);
+char *export(char **filenames, int open);
+int bulk_rename(char **args);
+int remove_file(char **args);
+int copy_function(char **comm);
+int edit_link(char *link);
+int open_function(char **cmd);
+int xchmod(const char *file, mode_t mode);
+int create_file(char **cmd);
