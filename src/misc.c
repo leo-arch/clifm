71f58722b64a8d01744a42a1d7023f262c779d55
leo-arch <leonardoabramovich2@gmail.com>
2021-06-13 03:09:02 -0300

    Update manpage

--- src/misc.c
+++ src/misc.c
@@ -1604,7 +1604,7 @@ help_function(void)
  cl, columns [on, off]\n\
  cmd, commands\n\
  cs, colorscheme [edit] [COLORSCHEME]\n\
- d, dup SOURCE DEST\n\
+ d, dup SOURCE [DEST]\n\
  ds, desel [*, a, all]\n\
  edit [APPLICATION]\n\
  exp, export [ELN/FILE ...]\n\
