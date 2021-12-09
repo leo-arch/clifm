6530217a440663c93347cd935657d2e6cfff9854
Docbroke <docbroke@protonmail.com>
2021-12-09 22:08:01 +0530

    Update fzcd.sh

--- plugins/fzcd.sh
+++ plugins/fzcd.sh
@@ -5,6 +5,7 @@
 DIR="$(find . -type d 2> /dev/null |\
 fzf --prompt "Change DIR :" \
 --reverse --border --height 15 \
+--bind "right:accept" \
 --color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4
 )"
 
