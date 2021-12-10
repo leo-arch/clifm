1f3cd3cf2e3db9606d8789c68650f2e1eb52f44c
leo-arch <leonardoabramovich2@gmail.com>
2021-12-09 21:00:26 -0300

    Fix shellcheck warnings

--- plugins/jumper.sh
+++ plugins/jumper.sh
@@ -3,7 +3,7 @@
 # CliFM plugin to navigate the jump database via fzf/Rofi
 # Written by L. Abramovich
 
-if [ -n "$1" ] && ([ "$1" = "--help" ] || [ "$1" = "help" ]); then
+if [ -n "$1" ] && { [ "$1" = "--help" ] || [ "$1" = "help" ]; }; then
 	name="$(basename "$0")"
 	printf "Navigate CLiFM jump database via FZF or Rofi. Press Enter to cd into the selected directory\n"
 	printf "Usage: %s\n" "$name"
