cccbb636b5cd29d5ef769b989d69cdd4fde3df01
leo-arch <leonardoabramovich2@gmail.com>
2021-12-12 03:03:37 -0300

    Improve mime plugin with FZF

--- plugins/mime_list.sh
+++ plugins/mime_list.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-# Plugin to list files in CWD by a given mime type
+# Plugin to list files in CWD by a given mime type using FZF
 # Written by L. Abramovich
+# License GPL3
 
 mime=""
 
@@ -12,11 +13,21 @@ if [ -n "$1" ] && { [ "$1" = "--help" ] || [ "$1" = "help" ]; }; then
 	exit 0
 fi
 
+if [ ! "$(which fzf 2>/dev/null)" ]; then
+	printf "CliFM: fzf: Command not found\n"
+	exit 1
+fi
+
+printf "Enter a MIME type or part of it. Ex: image\n"
 while [ -z "$mime" ]; do
 	printf "Mime type: "
 	read -r mime
 done
 
-find . -maxdepth 1 -mindepth 1 | file -F'@' -N -n --mime-type -if- | grep "@\ .*${mime}" | cut -d"@" -f1 | cut -d"/" -f2-10 | sort
+find . -maxdepth 1 -mindepth 1 | \
+file -F'@' -N -n --mime-type -if- | \
+grep "@\ .*${mime}" | cut -d"@" -f1 | cut -d"/" -f2-10 | sort | \
+fzf --reverse --height=15 \
+--info=inline --color=fg+:reverse,bg+:236,prompt:6,pointer:2,marker:2:bold,spinner:6:bold
 
 exit 0
