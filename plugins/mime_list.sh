bf39461b6f9f8586d2c58af43623a5331d67414e
leo-arch <leonardoabramovich2@gmail.com>
2021-12-12 03:15:19 -0300

    Mime plugin: Exit immediately if no results

--- plugins/mime_list.sh
+++ plugins/mime_list.sh
@@ -18,16 +18,18 @@ if [ ! "$(which fzf 2>/dev/null)" ]; then
 	exit 1
 fi
 
-printf "Enter a MIME type or part of it. Ex: image\n"
+printf "Enter a MIME type or part of it ('q' to quit). Ex: image\n"
 while [ -z "$mime" ]; do
 	printf "Mime type: "
 	read -r mime
 done
 
+{ [ "$mime" = "q" ] || [ "$mime" = "quit" ] ; } && exit 0
+
 find . -maxdepth 1 -mindepth 1 | \
 file -F'@' -N -n --mime-type -if- | \
 grep "@\ .*${mime}" | cut -d"@" -f1 | cut -d"/" -f2-10 | sort | \
-fzf --reverse --height=15 \
+fzf --reverse --height=15 --exit-0 \
 --info=inline --color=fg+:reverse,bg+:236,prompt:6,pointer:2,marker:2:bold,spinner:6:bold
 
 exit 0
