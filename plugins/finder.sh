6f159a97d600a61f81e700411f53fffa0c1fdc38
leo-arch <leonardoabramovich2@gmail.com>
2022-01-01 09:05:46 -0300

    Fix fzf in finder plugin

--- plugins/finder.sh
+++ plugins/finder.sh
@@ -67,7 +67,7 @@ if [ "$finder" = "fzf" ]; then
 	# shellcheck disable=SC2154
 	FILE="$($ls_cmd | fzf --ansi --prompt "$fzf_prompt" \
 	--reverse --height "$fzf_height" --info=inline \
-	--header "Find files in the current directory"
+	--header "Find files in the current directory" \
 	--bind "tab:accept" --info=inline --color="$(get_fzf_colors)")"
 else
 	# shellcheck disable=SC2012
