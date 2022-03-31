236c3269b4181b446b9d2cd84a3f62993d1ac88c
leo-arch <leonardoabramovich2@gmail.com>
2022-03-31 19:39:48 -0300

    Fix music player plugin

--- plugins/music_player.sh
+++ plugins/music_player.sh
@@ -4,21 +4,19 @@
 # Written by L. Abramovich
 # License: GPL3
 
-SUCCESS=0
-ERROR=1
 PLAYER="mplayer"
-#OPTS="-playlist"
+OPTS="-playlist"
 
-if [ -z "$1" ] || [ "$1" = "--help" ] || [ "$1" = "help" ]; then
+if [ -z "$1" ] || [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
 	name="${CLIFM_PLUGIN_NAME:-$(basename "$0")}"
 	printf "Play music files\n"
 	printf "Usage: %s FILE(s)\n" "$name"
-	exit $SUCCESS
+	exit 0
 fi
 
-if ! [ "$(which mplayer 2>/dev/null)" ]; then
-	printf "CliFM: mplayer: Command not found\n" >&2
-	exit $ERROR;
+if ! type "$PLAYER" >/dev/null 2>&1; then
+	printf "CliFM: %s: Command not found\n" "$PLAYER" >&2
+	exit 127;
 fi
 
 TMP_FILE="/tmp/clifm/playlist.$(tr -dc A-Za-z0-9 </dev/urandom | head -c 6)"
@@ -31,10 +29,10 @@ for file in "$@"; do
 	fi
 done
 
-"${EDITOR:=nano}" "$TMP_FILE"
+"${EDITOR:-nano}" "$TMP_FILE"
 
-"$PLAYER" "$OPS" "$TMP_FILE"
+"$PLAYER" "$OPTS" "$TMP_FILE"
 
 rm -rf -- "$TMP_FILE"
 
-exit $SUCCESS
+exit 0
