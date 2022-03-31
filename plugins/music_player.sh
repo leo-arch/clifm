9060fd4dbcd8a80b87565c3c981ab66550849cba
leo-arch <leonardoabramovich2@gmail.com>
2022-03-31 19:49:03 -0300

    Update music player plugin description

--- plugins/music_player.sh
+++ plugins/music_player.sh
@@ -9,7 +9,7 @@ OPTS="-playlist"
 
 if [ -z "$1" ] || [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
 	name="${CLIFM_PLUGIN_NAME:-$(basename "$0")}"
-	printf "Play music files\n"
+	printf "Create a playlist using FILE(s) and play it\n"
 	printf "Usage: %s FILE(s)\n" "$name"
 	exit 0
 fi
