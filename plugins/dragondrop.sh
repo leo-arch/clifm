637d220454f32d6723897e269cbcd7969d9c5961
leo-arch <leonardoabramovich2@gmail.com>
2022-03-31 20:21:25 -0300

    Update dragondrop plugin

--- plugins/dragondrop.sh
+++ plugins/dragondrop.sh
@@ -15,27 +15,24 @@
 # then send to the SelBox, whereas files dropped locally are directly
 # send to the SelBox
 
-SUCCESS=0
-ERROR=1
-
 if [ -n "$1" ] && { [ "$1" = "--help" ] || [ "$1" = "help" ]; }; then
 	name="${CLIFM_PLUGIN_NAME:-$(basename "$0")}"
 	printf "Drag and drop files\n"
 	printf "\nUsage: %s [FILE... n]\n" "$name"
 	printf "\nWith no arguments, it opens a window to drop files (using dragon); otherwise, files passed as arguments are send to the Dragon window to be dragged onto somewhere else.\n"
-	exit $SUCCESS
+	exit 0
 fi
 
 
 DRAGON=""
 
-if [ "$(which dragon-drag-and-drop 2>/dev/null)" ]; then
+if type dragon-drag-and-drop >/dev/null 2>&1; then
 	DRAGON="dragon-drag-and-drop"
-elif [ "$(which dragon 2>/dev/null)" ]; then
+elif type dragon >/dev/null 2>&1; then
 	DRAGON="dragon"
 else
 	printf "CliFM: Neither dragon nor dragon-drag-and-drop were found. Exiting...\n" >&2
-	exit $ERROR
+	exit 1
 fi
 
 if [ -z "$1" ]; then
@@ -47,14 +44,13 @@ if [ -z "$1" ]; then
 		else
 			printf "%s\n" "$r" >> "$CLIFM_SELFILE"
 		fi
-
 	done)
 
-	[ "$ret" = 0 ] && exit $SUCCESS
+	[ "$ret" = 0 ] && exit 0
 else
 	if $DRAGON "$@"; then
-		exit $SUCCESS
+		exit 0
 	fi
 fi
 
-exit $ERROR
+exit 1
