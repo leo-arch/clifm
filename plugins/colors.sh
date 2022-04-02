f3e50f93089970c4e895a0476c7689fdd5626a61
leo-arch <leonardoabramovich2@gmail.com>
2022-04-02 12:16:32 -0300

    Improve the colors test plugin

--- plugins/colors.sh
+++ plugins/colors.sh
@@ -8,16 +8,15 @@ name="${CLIFM_PLUGIN_NAME:-$(basename "$0")}"
 
 USAGE="Usage: $name [16, 256, test]"
 
-if [ -z "$1" ] || [ "$1" = "--help" ]; then
+if [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
 	printf "Terminal colors test script for CliFM\n"
-	printf "%s\n" "$USAGE" >&2
+	printf "Usage: %s [16, 256, test]\n" "$name"
 	exit 1
 fi
 
-opt="$1"
+opt="${1:-test}"
 
 if [ "$opt" = "256" ]; then
-
 	printf "256 Colors\n"
 	for fgbg in $(seq 38 48) ; do
 		for color in $(seq 0 256); do #Colors
@@ -30,7 +29,6 @@ if [ "$opt" = "256" ]; then
 	done
 
 elif [ "$opt" = "16" ]; then
-
 	printf "16 colors\n"
 	for clbg in $(seq 40 47) $(seq 100 107) 49 ; do
 	        for clfg in $(seq 30 37) $(seq 90 97) 39 ; do
@@ -48,13 +46,13 @@ elif [ "$opt" = "test" ]; then
 	printf "\e[00;32mtest\e[0m\n\n"
 
 	printf "16 colors (4-bit)\n"
-	printf "\e[01;32mtest\e[0m\n\n"
+	printf "\e[00;32mtest\e[0m\n\n"
 
 	printf "256 colors (8-bit)\n"
-	printf "\e[01;38;5;82mtest\e[0m\n\n"
+	printf "\e[00;38;5;2mtest\e[0m\n\n"
 
 	printf "RGB, true colors (24-bit)\n"
-	printf "\e[01;38;2;0;255;0mtest\e[0m\n\n"
+	printf "\e[00;38;2;20;170;0mtest\e[0m\n\n"
 
 else
 	printf "%s\n" "$USAGE" >&2
