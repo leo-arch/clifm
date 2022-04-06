f3a66d0729c556418056631fb3f4fb57745787b6
leo-arch <leonardoabramovich2@gmail.com>
2022-04-05 23:57:07 -0300

    Bump version to 1.5.2

--- src/settings.h
+++ src/settings.h
@@ -28,11 +28,11 @@
 #define PROGRAM_NAME "CliFM"
 #define PNL "clifm" /* Program name lowercase */
 #define PROGRAM_DESC "The command line file manager"
-#define VERSION "1.5.1"
+#define VERSION "1.5.2"
 #define AUTHOR "L. Abramovich"
 #define CONTACT "johndoe.arch@outlook.com"
 #define WEBSITE "https://github.com/leo-arch/clifm"
-#define DATE "Mar 26, 2022"
+#define DATE "Apr 5, 2022"
 #define LICENSE "GPL2+"
 #define COLORS_REPO "https://github.com/leo-arch/clifm-colors"
 
