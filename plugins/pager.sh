9d75d9d78489676b4c7b19d9446d581f96b59eeb
leo-arch <leonardoabramovich2@gmail.com>
2022-06-17 00:07:17 -0300

    Update pager plugin

--- plugins/pager.sh
+++ plugins/pager.sh
@@ -25,32 +25,15 @@ if ! type "$_pager" >/dev/null 2>&1; then
 fi
 
 if [ "$_pager" = "less" ]; then
-	_pager_opts="-ncs -PCliFM --tilde"
+	_pager_opts="-ncs -Pclifm --tilde"
 fi
 
-TMP_DIR="${TMPDIR:-/tmp}"
-TMP_FILE=$(mktemp "$TMP_DIR/clifm_pager.XXXXXX")
-
 # This produces a columned but uncolored list of files
-clifm --no-color --no-columns --list-and-quit --no-clear-screen "$PWD" > "$TMP_FILE"
+clifm --no-color --no-columns --list-and-quit --no-clear-screen "$PWD" | column | "$_pager" $_pager_opts
 
-# shellcheck disable=SC2086
-while read -r entry; do
-	printf "%s\n" "$entry"
-done < "$TMP_FILE" | column | "$_pager" $_pager_opts
-
-# To get a colored but uncolumned list of files, uncomment these lines and comment out the above ones
-# We use here most instead of less, since it supports color
-
-#clifm --no-columns --list-and-quit --no-clear-screen "$PWD" > "$TMP_FILE"
-#
-## shellcheck disable=SC2086
-#while read -r entry; do
-#	printf "%s \n" "$entry"
-#done < "$TMP_FILE" | most
+# To get a colored but uncolumned list of files, uncomment this line and comment out the one above
+#clifm --list-and-quit --no-columns --no-clear-screen "$PWD" | most
 
 # Ideally, we want a columned AND colored list, I know, but this isn't working right now
-
-rm -- "$TMP_FILE"
 
 exit 0
