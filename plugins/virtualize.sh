74cbef514d643811dea5ce04e75d686ae4093236
leo-arch <leonardoabramovich2@gmail.com>
2022-07-18 20:35:12 -0300

    Update virtual dirs plugin

--- plugins/virtualize.sh
+++ plugins/virtualize.sh
@@ -4,6 +4,12 @@
 # Author: L. Abramovich
 # License: GPL3
 
+# Dependencies: sed
+
+# Note: A new instance of CliFM will be spawned on a new terminal window
+# using $term_cmd, which defaults to 'xterm -e'. Edit this variable to
+# use your favorite terminal emulator instead
+
 if [ -z "$1" ] || [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
 	name="${CLIFM_PLUGIN_NAME:-$(basename "$0")}"
 	printf "Create a virtual directory for a set of files\n\
@@ -12,9 +18,13 @@ Usage: %s [-f,--full-paths] FILE... \n\n\
 	exit 0
 fi
 
-term_cmd="xterm -e"
+if ! type sed > /dev/null 2>&1; then
+	printf "clifm: sed: Command not found\n" >&2
+	exit 127
+fi
 
-clifm="clifm"
+term_cmd="xterm -e"
+clifm_bin="clifm"
 clifm_opts=""
 
 if [ "$1" = "-f" ] || [ "$1" = "--full-paths" ]; then
@@ -27,7 +37,7 @@ fi
 # 3. Replace tabs (first step) by spaces
 # 4. Remove remaining escape chars
 files="$(echo "$*" | sed 's/\\ /\t/g' | sed 's/ /\n/g' | sed 's/\t/ /g' | sed 's/\\//g')"
-cmd="$term_cmd 'echo \"$files\" | $clifm $clifm_opts'"
+cmd="$term_cmd 'echo \"$files\" | $clifm_bin $clifm_opts'"
 
 eval "$cmd"
 
