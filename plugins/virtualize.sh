ce3159c0e1e66b51533c21579bbefb4532a5601a
leo-arch <leonardoabramovich2@gmail.com>
2022-07-20 03:15:34 -0300

    Update virtual dirs plugin

--- plugins/virtualize.sh
+++ plugins/virtualize.sh
@@ -13,8 +13,7 @@
 if [ -z "$1" ] || [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
 	name="${CLIFM_PLUGIN_NAME:-$(basename "$0")}"
 	printf "Create a virtual directory for a set of files\n\
-Usage: %s [-f,--full-paths] FILE... \n\n\
--f,--full-paths     List FILE(s) as full paths instead of just base names\n" "$name" >&2
+Usage: %s FILE... \n" "$name" >&2
 	exit 0
 fi
 
@@ -26,11 +25,6 @@ fi
 term_cmd="xterm -e"
 clifm_bin="clifm"
 clifm_opts=""
-
-if [ "$1" = "-f" ] || [ "$1" = "--full-paths" ]; then
-	clifm_opts="$clifm_opts --virtual-dir-full-paths"
-	shift
-fi
 
 # 1. Replace escaped spaces by tabs
 # 2. Replace non-escaped spaces by new line chars
