bf6519a7f3d7a062c4c01e47c5a5333001bff65a
leo-arch <leonardoabramovich2@gmail.com>
2022-07-22 04:15:51 -0300

    Fix file picker function for files with spaces

--- functions/file_picker.sh
+++ functions/file_picker.sh
@@ -1,15 +1,20 @@
 # shellcheck shell=sh
 
-# CliFM file picker function
+# File picker function for CliFM
 
 # Usage: p
-
+#
 # 1. CliFM will be executed: select the files you need and quit.
 # 2. You will be asked for a command to execute over selected files,
 # for example: ls -l
-
+#
 # Customize this function as you need and add it to your shell rc file.
 # Recall to restart your shell for changes to take effect.
+
+# Dependecies: clifm, sed, find, xargs
+
+# Author: L. Abramovich
+# License: GPL3
 
 p() {
 	# Options to be passed to clifm
@@ -17,20 +22,20 @@ p() {
 
 	# shellcheck disable=SC2086
 	clifm $CLIFM_OPTIONS
-	file="$(find "${XDG_CONFIG_HOME:=${HOME}/.config}/clifm" -name 'selbox*')"
-	if [ -z "$file" ]; then
-		file="$(find /tmp/clifm -name 'selbox*')"
+	sel_file="$(find "${XDG_CONFIG_HOME:=${HOME}/.config}/clifm" -name 'selbox*')"
+	if [ -z "$sel_file" ]; then
+		sel_file="$(find /tmp/clifm -name 'selbox*')"
 	fi
 
-	if [ -f "$file" ]; then
+	if [ -f "$sel_file" ]; then
 		cmd=""
 		while [ -z "$cmd" ]; do
 			printf "Enter command ('q' to quit): "
 			read -r cmd
 		done
-		# shellcheck disable=SC2046
-		[ "$cmd" != "q" ] && $cmd $(sed 's/ /\\ /g' "$file")
-		rm -- "$file"
+		# shellcheck disable=SC2086
+		[ "$cmd" != "q" ] && sed 's/ /\\ /g' "$sel_file" | xargs $cmd
+		rm -- "$sel_file"
 	else
 		printf "No selected files\n" >&2
 	fi
