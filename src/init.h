b306e7ea558076cdd52df31960cd1ecf8b61a53f
leo-arch <leonardoabramovich2@gmail.com>
2022-08-12 08:53:51 -0300

    Alphabetically sort functions in init.h

--- src/init.h
+++ src/init.h
@@ -25,39 +25,37 @@
 #ifndef INIT_H
 #define INIT_H
 
+int backup_argv(int argc, char **argv);
 void check_env_filter(void);
-void get_prompt_cmds(void);
-void get_aliases(void);
-int get_last_path(void);
 void check_options(void);
-int load_dirhist(void);
-size_t get_path_env(void);
+void external_arguments(int argc, char **argv);
+void get_aliases(void);
 size_t get_cdpath(void);
+void get_data_dir(void);
+char *get_date(void);
+int get_home(void);
+int get_last_path(void);
+size_t get_path_env(void);
+void get_path_programs(void);
 void get_prompt_cmds(void);
 int get_sel_files(void);
+int get_sys_shell(void);
+struct user_t get_user_data(void);
+int init_gettext(void);
+int init_history(void);
 void init_shell(void);
-void load_jumpdb(void);
-int load_bookmarks(void);
+int init_workspaces(void);
 int load_actions(void);
+int load_bookmarks(void);
+int load_dirhist(void);
+void load_jumpdb(void);
 int load_pinned_dir(void);
 int load_prompts(void);
 int load_remotes(void);
 void load_tags(void);
-void get_path_programs(void);
-void unset_xargs(void);
-void external_arguments(int argc, char **argv);
-char *get_date(void);
-void get_data_dir(void);
-int set_start_path(void);
-int init_history(void);
-int init_workspaces(void);
-int init_gettext(void);
-int get_home(void);
-int backup_argv(int argc, char **argv);
-int get_sys_shell(void);
 void set_prop_fields(char *line);
+int set_start_path(void);
+void unset_xargs(void);
 /*int xsecure_env(const int mode); */
-
-struct user_t get_user_data(void);
 
 #endif /* INIT_H */
