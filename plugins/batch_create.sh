24f29d62ea51f947930b597260659eda85bfa31e
leo-arch <leonardoabramovich2@gmail.com>
2022-09-22 12:51:35 -0300

    Fix batch create plugin unmatched quote error

--- plugins/batch_create.sh
+++ plugins/batch_create.sh
@@ -6,17 +6,22 @@
 
 # Dependencies: xargs, text editor
 
-if [ -n "$1" ] && [ "$1" = "--help" ]; then
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
 	name="${CLIFM_PLUGIN_NAME:-$(basename "$0")}"
-	printf "Open a text editor to add files and/or directories to be created (file names endig with a slash will be taken as directory names). After saving the file and closing the editor, files will be created."
+	printf "Open a text editor to add files and/or directories to be created (file names endig with a slash will be taken as directory names). After saving the file and closing the editor, files will be created (via mkdir(1) and touch(1))."
 	printf "Usage: %s\n" "$name"
 	exit 0
 fi
 
+if [ -z "$EDITOR" ] && ! type nano >/dev/null 2>&1; then
+	printf "No editor specified. Set the EDITOR environment variable. Ex: export EDITOR=vim\n" >&2
+	exit 1
+fi
+
 TMP_DIR="${TMPDIR:-/tmp}"
-TMP=$(mktemp "$TMP_DIR/clifm_bn.XXXXXX")
-FILES=$(mktemp "$TMP_DIR/clifm_bn.XXXXXX")
-DIRS=$(mktemp "$TMP_DIR/clifm_bn.XXXXXX")
+TMP=$(mktemp "$TMP_DIR/clifm_bn_tmp.XXXXXX")
+FILES=$(mktemp "$TMP_DIR/clifm_bn_files.XXXXXX")
+DIRS=$(mktemp "$TMP_DIR/clifm_bn_dirs.XXXXXX")
 
 dirsn=0
 filesn=0
@@ -35,10 +40,10 @@ while read -r line; do
 	fi
 
 	if [ "$(printf "%s" "$line" | tail -c1)" = "/" ]; then
-		printf "%s\n" "$line" >> "$DIRS"
+		printf "%s\0" "$line" >> "$DIRS"
 		dirsn=$((dirsn + 1))
 	else
-		printf "%s\n" "$line" >> "$FILES"
+		printf "%s\0" "$line" >> "$FILES"
 		case "$line" in
 			*/*) mkdir -p "${line%/*}" ;;
 		esac
@@ -48,11 +53,11 @@ while read -r line; do
 done < "$TMP"
 
 if [ $dirsn -gt 0  ]; then
-	xargs -I{} mkdir -p {} < "$DIRS"
+	xargs -0 -I{} mkdir -p {} < "$DIRS"
 fi
 
 if [ $filesn -gt 0 ]; then
-	xargs -I{} touch {} < "$FILES"
+	xargs -0 -I{} touch {} < "$FILES"
 fi
 
 rm -f -- "$TMP" "$FILES" "$DIRS" 2>/dev/null
