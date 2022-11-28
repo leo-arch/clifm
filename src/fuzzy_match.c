4ed8ab431c914b47fa8d72aeeebb4d3f561b28f4
leo-arch <leonardoabramovich2@gmail.com>
2022-11-28 03:46:45 -0300

    Fix fuzzy match for uppercase in middle of string

--- src/fuzzy_match.c
+++ src/fuzzy_match.c
@@ -232,10 +232,11 @@ fuzzy_match(char *s1, char *s2, const size_t s1_len, const int type)
 			return 0;
 	}
 
+	int cs = conf.case_sens_path_comp;
 	int included = 0;
 	char *p = (char *)NULL;
 
-	if (conf.case_sens_path_comp == 1 ? (p = strstr(s2, s1)) : (p = strcasestr(s2, s1))) {
+	if (cs == 1 ? (p = strstr(s2, s1)) : (p = strcasestr(s2, s1))) {
 		if (p == s2) {
 			if (!*(s2 + s1_len))
 				return EXACT_MATCH_BONUS;
@@ -248,7 +249,7 @@ fuzzy_match(char *s1, char *s2, const size_t s1_len, const int type)
 	int word_beginning = 0;
 	int consecutive_chars = 0;
 	int first_char = 0;
-	if (conf.case_sens_path_comp == 1)
+	if (cs == 1)
 		first_char = *s1 == *s2 ? 1 : 0;
 	else
 		first_char = TOUPPER(*s1) == TOUPPER(*s2) ? 1 : 0;
@@ -257,11 +258,12 @@ fuzzy_match(char *s1, char *s2, const size_t s1_len, const int type)
 	char *hs = s2;
 
 	while (*s1) {
-		char *m = conf.case_sens_path_comp == 1 ? strchr(hs, *s1) : xstrcasechr(hs, *s1);
+		char *m = cs == 1 ? strchr(hs, *s1) : xstrcasechr(hs, *s1);
 		if (!m)
 			break;
 
-		if (*(s1 + 1) && *(m + 1) && *(s1 + 1) == *(m + 1))
+		if (*(s1 + 1) && *(m + 1) && (cs == 1 ? *(s1 + 1) == *(m + 1)
+		: TOUPPER(*(s1 + 1)) == TOUPPER(*(m + 1)) ))
 			consecutive_chars++;
 
 		if (l > 0 && (!IS_ALPHA_CASE(*(m - 1)) || IS_CAMEL_CASE(*m, *(m - 1)) ) )
