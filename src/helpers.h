a92b34323b29d484a9330433449c6d286bd43300
leo-arch <leonardoabramovich2@gmail.com>
2022-12-04 22:00:56 -0300

    Bump version to 1.9-rc3

--- src/helpers.h
+++ src/helpers.h
@@ -113,10 +113,10 @@
 #define PROGRAM_NAME "clifm"
 #define PNL "clifm" /* Program name lowercase */
 #define PROGRAM_DESC "The command line file manager"
-#define VERSION "1.9-rc2"
+#define VERSION "1.9-rc3"
 #define AUTHOR "L. Abramovich"
 #define CONTACT "https://github.com/leo-arch/clifm"
-#define DATE "Dec 02, 2022"
+#define DATE "Dec 04, 2022"
 #define LICENSE "GPL2+"
 #define COLORS_REPO "https://github.com/leo-arch/clifm-colors"
 
