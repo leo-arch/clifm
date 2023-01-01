f77af0ff853d7809ebdcadc3c6669799abf0f93a
leo-arch <leonardoabramovich2@gmail.com>
2023-01-01 04:55:57 -0300

    Add license notice to icons files

--- src/icons-emoji.h
+++ src/icons-emoji.h
@@ -1,4 +1,31 @@
-/* icons-emoji.h - Icons definitions for CliFM */
+/* icons-emoji.h - Icon definitions for CliFM */
+
+/* Taken from
+ * https://github.com/jarun/nnn, licensed under BSD-2-Clause.
+ * All changes are licensed under GPL-2.0-or-later.
+*/
+
+/*
+ * This file is part of CliFM
+ *
+ * Copyright (C) 2016-2023, L. Abramovich <johndoe.arch@outlook.com>
+ * All rights reserved.
+
+ * CliFM is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * CliFM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+*/
 
 /* For a list of emojis consult:
  * https://unicode.org/Public/emoji/5.0/emoji-test.txt */
--- src/icons-nerdfont.h
+++ src/icons-nerdfont.h
@@ -1,4 +1,31 @@
-/* icons-nerdfont.h - Icons defintions for CliFM */
+/* icons-nerdfont.h - Icon defintions for CliFM */
+
+/* Taken from
+ * https://github.com/jarun/nnn, licensed under BSD-2-Clause.
+ * All changes are licensed under GPL-2.0-or-later.
+*/
+
+/*
+ * This file is part of CliFM
+ *
+ * Copyright (C) 2016-2023, L. Abramovich <johndoe.arch@outlook.com>
+ * All rights reserved.
+
+ * CliFM is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * CliFM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+*/
 
 /* For more codepoints see https://www.nerdfonts.com/cheat-sheet */
 
