9ca41c87981a7bbed40025de7370a460150e0d6a
leo-arch <leonardoabramovich2@gmail.com>
2023-02-15 23:59:36 -0300

    Add Wayland support for the wallpaper setter plugin

--- plugins/wallpaper_setter.sh
+++ plugins/wallpaper_setter.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 # Wallpaper setter plugin for CliFM
-# Dependencies: file, and feh, xsetbg (xloadimage), or hsetroot
+# Dependencies:
+#  file, and
+#    feh, xsetbg (xloadimage), hsetroot, or nitrogen (for X)
+#    swww (https://github.com/Horus645/swww) or swaybg (for Wayland)
 
 # Written by L. Abramovich
 # License: GPL3
@@ -11,6 +14,11 @@ if [ -z "$1" ] || [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
 	printf "Set IMAGE as wallpaper\n"
 	printf "\n\x1b[1mUSAGE\x1b[0m\n  %s IMAGE\n" "$name"
 	exit 0
+fi
+
+if [ -z "$DISPLAY" ]; then
+	printf "clifm: A graphical environment is required\n" >&2
+	exit 1
 fi
 
 
@@ -24,22 +32,25 @@ if ! file -bi "$1" | grep -q "image/"; then
 	exit 1
 fi
 
-if type feh >/dev/null 2>&1; then
-	if feh --no-fehbg --bg-fill "$1"; then
-		exit 0
-	fi
-elif type xsetbg >/dev/null 2>&1; then
-	if xsetbg -center "$1"; then
-		exit 0
-	fi
-elif type hsetroot >/dev/null 2>&1; then
-	if hsetroot -center "$1"; then
+if [ -n "$WAYLAND_DISPLAY" ]; then
+	if type swww >/dev/null 2>&1; then
+		swww img "$1" && exit 0
+	elif type swaybg >/dev/null 2>&1; then
+		killall swaybg 2>/dev/null
+		swaybg -m fill -i "$1" 2>/dev/null &
 		exit 0
 	fi
 else
-	printf "CliFM: No wallpaper setter found. Install either feh, \
-xloadimage, or hsetroot\n" >&2
-	exit 1
+	if type feh >/dev/null 2>&1; then
+		feh --no-fehbg --bg-fill "$1" && exit 0
+	elif type xsetbg >/dev/null 2>&1; then
+		xsetbg -center "$1" && exit 0
+	elif type hsetroot >/dev/null 2>&1; then
+		hsetroot -center "$1" && exit 0
+	elif type nitrogen >/dev/null 2>&1; then
+		nitrogen --set-zoom-fill --save "$1" && exit 0
+	fi
 fi
 
+printf "clifm: Could not set wallpaper\n" >&2
 exit 1
