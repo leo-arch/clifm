b37a4ec1ba0a39ca660cc374f52792607f6c24ba
leo-arch <leonardoabramovich2@gmail.com>
2023-03-27 12:02:52 -0300

    Fix update.sh plugin

--- plugins/update.sh
+++ plugins/update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Plugins to check for CliFM's updates
+# Check clifm's upstream version
 # Dependencies: awk, grep
 
 # Written by L. Abramovich
@@ -8,24 +8,29 @@
 
 if [ -n "$1" ] && { [ "$1" = "--help" ] || [ "$1" = "-h" ]; }; then
 	name="${CLIFM_PLUGIN_NAME:-$(basename "$0")}"
-	printf "Check for CliFM updates\n"
+	printf "Check Clifm's upstream version\n"
 	printf "\n\x1b[1mUSAGE\x1b[0m\n  %s\n" "$name"
 	exit 0
 fi
 
-upstream="$(curl -s "https://github.com/leo-arch/clifm/releases/latest" | grep -Eo "[0-9]+\.[0-9]+.*" | cut -d'"' -f1)"
-
-if [ -z "$upstream" ]; then
-	printf "clifm: Error getting upstream version"
+if ! type clifm >/dev/null 2>&1 ; then
+	printf "Clifm is not installed\n" >&2
 	exit 1
 fi
 
-cur="$(clifm -v | awk 'NR==1{print $2}')"
+upstream="$(curl -s "https://api.github.com/repos/leo-arch/clifm/releases/latest" | grep tag_name | awk -F'"' '{print $4}')"
 
-if [ "$cur" = "$upstream" ]; then
+if [ -z "$upstream" ]; then
+	printf "clifm: Error fetching upstream version" >&2
+	exit 1
+fi
+
+cur="$(clifm -v)"
+
+if [ "v$cur" = "$upstream" ]; then
 	printf "You are up to date: %s is the latest release\n" "$cur"
 else
-	printf "%s: New release available\n" "$upstream"
+	printf "%s: New release available (current version is %s)\n" "$upstream" "$cur"
 fi
 
 exit 0
