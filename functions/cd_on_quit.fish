aa6dbf4aaf966d13846b03b29ead35bdb653d2ff
Spenser Black <spenserblack01@gmail.com>
2023-07-21 18:00:55 -0400

    Update headers

--- functions/cd_on_quit.fish
+++ functions/cd_on_quit.fish
@@ -1,6 +1,17 @@
-# Copy this into $HOME/.config/fish/functions/c.fish
-# Note that conventionally fish functions are named after the file they are in.
-# This works best with the fish built-in funced, funcsave, etc.
+# CliFM CD on quit
+
+# Description
+# Run CliFM and, if the exit command is "Q" (not "q"), read the .last file and cd into it.
+#
+# 1) Customize this function as needed and copy it into your fish functions directory as a .fish file with the same name.
+#    For example, with the default name, the file should be c.fish.
+# 2) Run clifm using the name of the function below: c [ARGS ...]
+
+# Dependecies: clifm, cut, grep
+
+# Author: Spenser Black
+# License: GPL3
+
 function c --wraps=clifm --description="Change directory after exiting clifm"
     clifm "--cd-on-quit" $argv
     set --query XDG_CONFIG_HOME; and set --function config_home "$XDG_CONFIG_HOME"; or set --function config_home "$HOME/.config"
--- functions/file_picker.fish
+++ functions/file_picker.fish
@@ -1,6 +1,20 @@
-# Copy this into $HOME/.config/fish/functions/p.fish
-# Note that conventionally fish functions are named after the file they are in.
-# This works best with the fish built-in funced, funcsave, etc.
+# File picker function for CliFM
+
+# Usage (assuming the function is not renamed): p [ARGS ...]
+#
+# 1. CliFM will be executed: select the files you need and quit.
+# 2. You will be asked for a command to execute over selected files,
+# for example: ls -l
+#
+# Customize this function as you need and copy it to your fish functions directory.
+# Note that conventionally fish functions are named after the file they are in, so
+# with the name below, the file should be named p.fish.
+
+# Dependecies: clifm, sed, find, xargs
+
+# Author: Spenser Black
+# License: GPL3
+
 function p --wraps=clifm --description="Execute a command on the files selected using CliFM"
     set --function clifm_selfile (mktemp "/tmp/clifm_selfile.XXXXXXXXXX")
 
