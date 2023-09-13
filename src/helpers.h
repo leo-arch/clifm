6d76e8dd02e6a9e2c342a10bc8b46116508de2e4
leo-arch <leonardoabramovich2@gmail.com>
2023-09-13 20:35:35 -0300

    Temporarily disable the legacy mode

--- src/helpers.h
+++ src/helpers.h
@@ -133,11 +133,11 @@
 #include <fcntl.h> /* AT_* constants (like AT_FDCWD) */
 /* Included here to test _DIRENT_HAVE_D_TYPE and DT macros. */
 #include <dirent.h>
-
+/*
 #if ((!defined(AT_FDCWD) || (defined(_POSIX_C_SOURCE) \
 && _POSIX_C_SOURCE < 200809L)) && !defined(CLIFM_LEGACY))
 # define CLIFM_LEGACY
-#endif
+#endif */
 
 #ifdef CLIFM_LEGACY
 /* Replace functions not available before POSIX-1.2008. More precisely,
