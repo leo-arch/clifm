81bf20510083534bfff1691c5d1ec6848c59ab17
leo-arch <leonardoabramovich2@gmail.com>
2023-12-06 15:28:44 -0300

    Remove commented code

--- src/aux.h
+++ src/aux.h
@@ -74,12 +74,6 @@ void xregerror(const char *, const char *, const int, const regex_t, const int);
 size_t hashme(const char *, const int);
 #endif /* !_NO_ICONS */
 
-/* Some memory wrapper functions */
-//void *xrealloc(void *, const size_t);
-//void *xnrealloc(void *, const size_t, const size_t);
-//void *xcalloc(const size_t, const size_t);
-//void *xnmalloc(const size_t, const size_t);
-
 __END_DECLS
 
 #endif /* AUX_H */
