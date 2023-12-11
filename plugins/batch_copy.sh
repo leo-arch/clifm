4c64aaa79928ebb3551ec972acbf233b1867788e
leo-arch <leonardoabramovich2@gmail.com>
2023-12-11 12:08:04 -0300

    Reformat bcp plugin help message

--- plugins/batch_copy.sh
+++ plugins/batch_copy.sh
@@ -12,9 +12,10 @@
 if [ -z "$1" ] || [ "$1" = "--help" ]; then
 	name="${CLIFM_PLUGIN_NAME:-$(basename "$0")}"
 	printf "Copy files into multiple directories at once\n"
-	printf "Usage: %s FILE...\n" "$name"
-	printf "\nExample:\nCopy file1 and file2 into dir1 and dir2 at once:\n\
-  %s file1 file2\n\nOnce in the editor, enter dir1 and dir2, save, and exit\n" "$name"
+	printf "\n\x1b[1mUSAGE\x1b[0m\n  %s FILE...\n" "$name"
+
+	printf "\n\x1b[1mEXAMPLE\x1b[0m\n- Copy file1 and file2 into dir1 and dir2 at once\n\
+    %s file1 file2\n\n  Once in the editor, enter dir1 and dir2, save, and exit.\n" "$name"
 	exit 0
 fi
 
@@ -22,8 +23,8 @@ DEST=$(mktemp "${TMPDIR:-/tmp}/clifm_bcd.XXXXXX")
 
 printf "# CliFM - Copy files in batch\n\
 # Write here destinty files/directories, one per line.\n\
-# Blank and commented lines are ommited\n\
-# Just quit the editor to cancel the operation\n" > "$DEST"
+# Blank and commented lines are omitted.\n\
+# Just quit the editor to cancel the operation.\n" > "$DEST"
 
 "${EDITOR-:nano}" "$DEST"
 
