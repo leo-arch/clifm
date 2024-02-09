2e346c8e2b58bca92596210506c2cc8d22d4956e
leo-arch <leonardoabramovich2@gmail.com>
2024-02-09 20:30:37 -0300

    Bump version to 1.17.1

--- src/helpers.h
+++ src/helpers.h
@@ -28,7 +28,7 @@
 #define PROGRAM_NAME_UPPERCASE "CliFM"
 #define PROGRAM_NAME "clifm"
 #define PROGRAM_DESC "The command line file manager"
-#define VERSION "1.17"
+#define VERSION "1.17.1"
 #define DATE "Feb 9, 2024"
 #define AUTHOR "L. Abramovich"
 #define CONTACT "https://github.com/leo-arch/clifm"
