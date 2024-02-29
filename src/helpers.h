690b74023fcc2634357c9c6a793d563794f6f79d
leo-arch <leonardoabramovich2@gmail.com>
2024-02-29 02:30:36 -0300

    Improve fileinfo struct alignment

--- src/helpers.h
+++ src/helpers.h
@@ -1250,83 +1250,43 @@ struct human_size_t {
 
 /* Struct to store files information */
 struct fileinfo {
-/*	struct human_size_t human_size;
-	struct groups_t uid_i;
-	struct groups_t gid_i;
-	char *color;
-	char *ext_color;
-	char *ext_name;
-	char *icon;
-	char *icon_color;
-	char *name;
-	filesn_t filesn;
-	blkcnt_t blocks;
-	size_t len;    // File name len (columns needed to display file name)
-	size_t bytes;  // Bytes consumed by file name
-	time_t ltime;  // For long view mode
-	time_t time;
-	ino_t inode;
-	off_t size;
-	nlink_t linkn; // 4 bytes in Solaris
-	uid_t uid;
-	gid_t gid;
-	mode_t mode;   // Store st_mode (for long view mode)
-	mode_t type;   // Store d_type value
-	int dir;
-	int eln_n;     // Amount of digits in ELN
-	int exec;
-	int ruser;     // User read permission for dir
-	int symlink;
-	int sel;
-	int xattr;
-	int du_status; // Exit status of du(1) for dir full sizes
-	int utf8;      // Name contains at least one UTF-8 character
-	int stat_err;  // stat(2) failed for this entry */
-	char *color;
-	char *ext_color;
-	char *ext_name;
-	char *icon;
-	char *icon_color;
-	struct groups_t uid_i;
-	struct groups_t gid_i;
 	struct human_size_t human_size;
+	struct groups_t uid_i;
+	struct groups_t gid_i;
+	char *color;
+	char *ext_color;
+	char *ext_name;
+	char *icon;
+	char *icon_color;
 	char *name;
 	filesn_t filesn;
-	size_t len;    // File name len (columns needed to display file name)
-	size_t bytes;  // Bytes consumed by file name
-	time_t ltime;  // For long view mode
+	blkcnt_t blocks;
+	size_t len;    /* File name len (columns needed to display file name) */
+	size_t bytes;  /* Bytes consumed by file name */
+	time_t ltime;  /* For long view mode */
 	time_t time;
 	ino_t inode;
-#if defined(__TERMUX__)
-	int pad0;
-#endif // __TERMUX__
 	off_t size;
-	nlink_t linkn; // 4 bytes in Solaris
-#if !defined(__sun)
-	int pad1;
-#endif // __sun
+	nlink_t linkn; /* 4 bytes on Solaris/BSD/HAIKU; 8 on Linux */
 	uid_t uid;
 	gid_t gid;
-	mode_t mode;   // Store st_mode (for long view mode)
-	mode_t type;   // Store d_type value
-#if !defined(__OpenBSD__) && !defined(__FreeBSD__) && !defined(__NetBSD__) \
-&& !defined(__HAIKU__) && (!defined(__arm__) || defined(__TERMUX__))
-	int pad2;
-#endif
-	blkcnt_t blocks;
+	mode_t mode;   /* Store st_mode (for long view mode) */
+	mode_t type;   /* Store d_type value */
 	int dir;
-	int eln_n;     // Amount of digits in ELN
+	int eln_n;     /* Amount of digits in ELN */
 	int exec;
-	int ruser;     // User read permission for dir
+	int ruser;     /* User read permission for dir */
 	int symlink;
 	int sel;
 	int xattr;
-	int du_status; // Exit status of du(1) for dir full sizes
-	int utf8;      // Name contains at least one UTF-8 character
-	int stat_err;  // stat(2) failed for this entry
-#if defined(__TERMUX__)
-	int pad3;
-#endif // __TERMUX__
+	int du_status; /* Exit status of du(1) for dir full sizes */
+	int utf8;      /* Name contains at least one UTF-8 character */
+	int stat_err;  /* stat(2) failed for this entry */
+#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) \
+|| defined(__APPLE__) || defined(__sun) || defined(__HAIKU__) \
+|| (defined(__arm__) && !defined(__ANDROID__))
+	int pad0;
+#endif
 };
 
 extern struct fileinfo *file_info;
