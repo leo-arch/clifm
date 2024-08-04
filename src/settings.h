67ddfc0af43f0207ffc8693717a84dafc16af5e2
leo-arch <leonardoabramovich2@gmail.com>
2024-08-04 13:44:29 -0300

    Improve default 4-bit date shades

--- src/settings.h
+++ src/settings.h
@@ -196,7 +196,7 @@ xf=1;31:xs=32:"
 #define DEF_DZ_C  "\x1b[0;32m" /* Size (only for dirs) */
 
 /* Default color shades for date field in file properties */
-#define DEF_DATE_SHADES_8   "1,31-2,36-1,36,36-2,36-2,36-2"
+#define DEF_DATE_SHADES_8   "1,31-2,37-1,37,37-2,36,36-2"
 
 /* Default color shades for size field in file properties */
 #define DEF_SIZE_SHADES_8   "1,31-2,36,32,33,31,35"
