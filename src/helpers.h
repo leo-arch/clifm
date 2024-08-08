5cdad3436f512efcc8fdf21ed7e53e62a2f18afd
leo-arch <leonardoabramovich2@gmail.com>
2024-08-08 00:16:53 -0300

    Improve explanation of the new MAX_INT_STR macro

--- src/helpers.h
+++ src/helpers.h
@@ -936,13 +936,15 @@ extern time_t curdir_mtime;
 #define TOLOWER(c) (((c) >= 'A' && (c) <= 'Z') ? ((c) - 'A' + 'a') : (c))
 
 /* A yottabyte takes 26 digits, 28 if we count the negative sign and the NULL
- * terminator, so that 32 bytes is more than enough. */
+ * terminator, so that 32 bytes is more than enough to represent any given
+ * integer as a string. */
 #define MAX_INT_STR 32
 /* Bytes required to transform an integer of type TYPE into a string,
  * including the ending NULL char and the negative sign. */
-/* CHAR_BIT is defined in limits.h */
+/* (CHAR_BIT is defined in limits.h). */
 /*#define INT_STR_SIZE(type) (((CHAR_BIT * sizeof(type) - 1) * 10 / 33) + 3) */
-/* INT_STR_SIZE() is much more accurate than MAX_INT_STR, but not fully tested. */
+/* INT_STR_SIZE() is much more accurate than MAX_INT_STR (which is just a
+ * big number) but not fully tested. */
 
 /* UINT_MAX is 4294967295 == 10 digits */
 #define DIGINUM(n) (((n) < 10) ? 1 \
