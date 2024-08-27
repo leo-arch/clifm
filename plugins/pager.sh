121f05b6b9f0f316679f1bc703d7843ae4b2a73c
leo-arch <leonardoabramovich2@gmail.com>
2024-08-27 13:47:08 -0300

    Update pager.sh plugin

--- plugins/pager.sh
+++ plugins/pager.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Description: Print the current list of files through a pager (PAGER or less)
-# Dependencies: less, column
+# Dependencies: less
 # Author: L. Abramovich
 # License: GPL3
 
@@ -17,15 +17,9 @@ if ! type column >/dev/null 2>&1; then
 	exit 127
 fi
 
-pager_cmd="${PAGER:-less}"
-
-if ! type "$pager_cmd" >/dev/null 2>&1; then
-	printf "clifm: %s: command not found\n" "$pager_cmd" >&2
+if ! type less >/dev/null 2>&1; then
+	printf "clifm: %s: command not found\n" "less" >&2
 	exit 127
-fi
-
-if [ "$pager_cmd" = "less" ]; then
-	pager_opts="-rncs -Pclifm --tilde"
 fi
 
 if [ "$CLIFM_LONG_VIEW" = "1" ]; then
@@ -35,6 +29,6 @@ else
 fi
 
 # shellcheck disable=SC2086
-CLIFM_COLUMNS="$(tput cols)" CLIFM_LINES="$(tput lines)" clifm $clifm_opts "$PWD" | "$pager_cmd" $pager_opts
+CLIFM_COLUMNS="$(tput cols)" CLIFM_LINES="$(tput lines)" clifm $clifm_opts "$PWD" | less -rncs -Pclifm --tilde
 
 exit 0
