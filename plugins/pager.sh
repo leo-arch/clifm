9976667b0cd8eec1617b00e0615e89d3dc201b44
leo-arch <leonardoabramovich2@gmail.com>
2024-08-27 14:57:28 -0300

    Remove unnecessary check

--- plugins/pager.sh
+++ plugins/pager.sh
@@ -1,24 +1,19 @@
 #!/bin/sh
 
-# Description: Print the current list of files through a pager (PAGER or less)
+# Description: Print the current list of files through a pager less(1)
 # Dependencies: less
 # Author: L. Abramovich
 # License: GPL3
 
 if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
 	name="${CLIFM_PLUGIN_NAME:-$(basename "$0")}"
-	printf "List the current list of files through a pager (PAGER or less)\n"
+	printf "List the current list of files through a pager (less)\n"
 	printf "\n\x1b[1mUSAGE\x1b[0m\n  %s\n" "$name"
 	exit 0
 fi
 
-if ! type column >/dev/null 2>&1; then
-	printf "clifm: column: command not found\n" >&2
-	exit 127
-fi
-
 if ! type less >/dev/null 2>&1; then
-	printf "clifm: %s: command not found\n" "less" >&2
+	printf "clifm: less: command not found\n" >&2
 	exit 127
 fi
 
