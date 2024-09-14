1f2b55c0d05d9b52e0dd9527e3e3a00609e2be6d
leo-arch <leonardoabramovich2@gmail.com>
2024-09-14 20:27:43 -0300

    Fix: gen_last_cmd_time() removes last digit

--- src/prompt.c
+++ src/prompt.c
@@ -874,20 +874,13 @@ gen_last_cmd_time(char **line)
 	if (last_cmd_time < (double)conf.prompt_b_min)
 		goto END;
 
-	if (conf.prompt_b_precision == 0) {
-		char *temp = xnmalloc(MAX_INT_STR, sizeof(char));
-		snprintf(temp, MAX_INT_STR, "%d",
-			(int)last_cmd_time < 0 ? INT_MAX : (int)last_cmd_time);
-		return temp;
-	}
-
-	const int precision = conf.prompt_b_precision + 1;
+	const int precision = conf.prompt_b_precision;
 	const int len = snprintf(NULL, 0, "%.*f", precision, last_cmd_time);
 	if (len < 0)
 		goto END;
 
-	char *temp = xnmalloc((size_t)len, sizeof(char));
-	snprintf(temp, (size_t)len, "%.*f", precision, last_cmd_time);
+	char *temp = xnmalloc((size_t)len + 1, sizeof(char));
+	snprintf(temp, (size_t)len + 1, "%.*f", precision, last_cmd_time);
 
 	return temp;
 
