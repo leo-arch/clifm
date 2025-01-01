d29c83db52b937dc10302fb8ab69e304badaa5cd
leo-arch <leonardoabramovich2@gmail.com>
2025-01-01 02:29:02 -0300

    Add license and author

--- functions/subshell_notice.sh
+++ functions/subshell_notice.sh
@@ -1,8 +1,12 @@
 # shellcheck shell=sh
 
 # Description: Change the shell prompt to notify the user that this
-# shell has been spawned from within CliFM
+# shell has been spawned from within Clifm
 
+# Author: L. Abramovich
+# License: GPL2+
+
+# Usage:
 # Copy the following line or source this file from your shell
 # configuration file (ex: .bashrc, .zshrc, etc)
 
