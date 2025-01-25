d724b434eaa50357e234a461389d59811645d33b
leo-arch <leonardoabramovich2@gmail.com>
2025-01-25 04:24:24 -0300

    Remove reference to GNU LS_COLORS

--- src/help.c
+++ src/help.c
@@ -77,7 +77,7 @@ print_tips(const int all)
 		"--secure-cmds command line switches",
 		"Run in disk usage analyzer mode using the -t command line switch",
 		"Fuzzy suggestions are supported. E.g.: 'dwn > Downloads'. Enable them via --fuzzy-matching, or FuzzyMatching in the configuration file",
-		"Miss LS_COLORS? Run with --lscolors (GNU ls only)",
+		"Miss LS_COLORS? Run with --lscolors",
 #else
 		"Create a new profile: 'pf add PROFILE' or 'clifm -p PROFILE'",
 		"Run in incognito/private mode via the -s flag",
