368be6f65a50b3462e0b8a3b23fbcf8e176e8949
leo-arch <leonardoabramovich2@gmail.com>
2025-02-11 20:31:48 -0300

    Preview x-setupscript files as text

--- src/config.c
+++ src/config.c
@@ -1122,7 +1122,7 @@ inode/directory=exa -a --tree --level=1;lsd -A --tree --depth=1 --color=always;t
 ^text/rtf=catdoc;\n\
 N:.*\\.json$=jq --color-output . ;python -m json.tool;\n\
 N:.*\\.md$=glow -s dark;mdcat;\n\
-^text/.*|^application/javascript$=highlight -f --out-format=xterm256 --force;bat --style=plain --color=always;cat;\n\
+^text/.*|^application/(javascript|x-setupscript)$=highlight -f --out-format=xterm256 --force;bat --style=plain --color=always;cat;\n\
 \n\
 # Office documents\n\
 N:.*\\.xlsx$=xlsx2csv;file -b;\n\
