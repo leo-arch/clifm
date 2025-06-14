32d32dacc75ae1e8ab5c81b0a4e05a790fc12bed
leo-arch <leonardoabramovich2@gmail.com>
2025-06-14 15:42:46 -0300

    Make array const

--- src/listing.c
+++ src/listing.c
@@ -281,7 +281,7 @@ swap_ent(const size_t id1, const size_t id2)
 static uint8_t
 is_utf8_name(const char *filename, size_t *bytes, size_t *ext_index)
 {
-	static unsigned char utf8_chars[256] = {
+	static const unsigned char utf8_chars[256] = {
 		/* Control characters */
 		[0] = 1, [1] = 1, [2] = 1, [3] = 1, [4] = 1, [5] = 1, [6] = 1, [7] = 1,
 		[8] = 1, [9] = 1, [10] = 1, [11] = 1, [12] = 1, [13] = 1, [14] = 1,
