8f8a7fc087cbdb83d439819bdc3ad3650f8fb7ab
leo-arch <leonardoabramovich2@gmail.com>
2025-09-29 11:00:46 -0300

    Fix build failure on Alpine/musl

--- src/translate_key.h
+++ src/translate_key.h
@@ -40,11 +40,15 @@
 #define CSI_INTRODUCER 0x5b /* [ */
 #define SS3_INTRODUCER 0x4f /* O */
 
-__BEGIN_DECLS
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 char *translate_key(char *str, const int term_type);
 int is_end_seq_char(unsigned char c);
 
-__END_DECLS
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* TRANSLATE_KEY_H */
