f191ee093bb88cd7cb2aa02663fb487f5956f73b
leo-arch <leonardoabramovich2@gmail.com>
2025-12-19 21:27:59 -0300

    Colorize no-perm icon for files differently than for dirs

--- src/listing.c
+++ src/listing.c
@@ -2902,7 +2902,8 @@ load_file_gral_info(const struct stat *a, const filesn_t n)
 		file_info[n].user_access = 0;
 #ifndef _NO_ICONS
 		file_info[n].icon = DEF_NOPERM_ICON;
-		file_info[n].icon_color = DEF_NOPERM_ICON_COLOR;
+		file_info[n].icon_color = file_info[n].dir == 1
+			? DEF_NOPERM_ICON_COLOR_DIR : DEF_NOPERM_ICON_COLOR_FILE;
 #endif /* !_NO_ICONS */
 	}
 
